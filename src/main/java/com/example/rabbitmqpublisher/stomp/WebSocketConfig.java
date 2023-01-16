e815bde9ae2624d671e30945e4e02f2fb3711ded
cykei <kelle111@naver.com>
2023-01-17 06:55:15 +0900

    rabbitmq + STOMP 설정 (연결실패)

--- src/main/java/com/example/rabbitmqpublisher/stomp/WebSocketConfig.java
+++ src/main/java/com/example/rabbitmqpublisher/stomp/WebSocketConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
-@Configuration
-@EnableWebSocketMessageBroker
+//@Configuration
+//@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
